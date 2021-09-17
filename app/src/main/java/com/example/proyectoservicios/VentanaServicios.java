9b84cfa3933aa0b9580234810d595233cb193863
Santiago Perez <santiago.grosso051600@hotmail.com>
2021-09-17 10:56:23 -0500

    este es mio XD

---
+++ app/src/main/java/com/example/proyectoservicios/VentanaServicios.java
@@ -0,0 +1,14 @@
+package com.example.proyectoservicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VentanaServicios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_servicios);
+    }
+}
--- app/src/main/java/com/example/proyectoservicios/holamundo.java
+++
@@ -1,4 +0,0 @@
-package com.example.proyectoservicios;
-
-public class holamundo {
-}
