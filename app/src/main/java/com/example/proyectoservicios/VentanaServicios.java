f2726b8a011c093d0d583fd251237fe17d57385c
Santiago Perez <santiago.grosso051600@hotmail.com>
2021-09-20 14:11:44 -0500

    botones dinamicos

--- app/src/main/java/com/example/proyectoservicios/VentanaServicios.java
+++ app/src/main/java/com/example/proyectoservicios/VentanaServicios.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -20,50 +22,61 @@ import java.util.ArrayList;
 public class VentanaServicios extends AppCompatActivity {
     LinearLayout contenedor;
     private DatabaseReference Database;
-    ArrayList<boton> lista = new ArrayList<boton>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ventana_servicios);
         contenedor = (LinearLayout)findViewById(R.id.contenedor);
         Database = FirebaseDatabase.getInstance().getReference();
+        ArrayList<boton> lista = new ArrayList<boton>();
         //setServiciosFromFirebase();
-        Database.child("servicios").addValueEventListener(new ValueEventListener() {
+
+        Database.child("servicios").addListenerForSingleValueEvent(new ValueEventListener() {
+
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
 
+                lista.add(new boton(1,"hola"));
+
+                if(snapshot.exists()){
                     for (DataSnapshot ds: snapshot.getChildren()) {
 
-                        lista.add(new boton(1,ds.child("nombre").getValue().toString()));
+                        lista.add(new boton(1,""+ds.child("nombre").getValue().toString()));
+
+
+                    }
+                    for(boton b:lista){
+                        Button bt = new Button(getApplicationContext());
+                        bt.setText(b.servicio);
+                        bt.setId(b.codigo);
+                        bt.setTextColor(Color.BLACK);
+                        contenedor.addView(bt);
+                        bt.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Log.d("STATE","HOLA------------------");
+                            }
+                        });
 
                     }
                 }
 
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                lista.add(new boton(1,"hola"));
             }
         });
 
 
-        for(boton b:lista){
-            Button bt = new Button(getApplicationContext());
-            bt.setText(b.servicio);
-            bt.setId(b.codigo);
-            bt.setTextColor(Color.BLACK);
-            contenedor.addView(bt);
-        }
+
     }
     private  void getServiciosFromFirebase(){
 
     }
     private  void setServiciosFromFirebase(){
-
         Database.child("servicios").push().child("nombre").setValue("Baño y peluquería");
-
     }
     class boton{
         public int codigo;
