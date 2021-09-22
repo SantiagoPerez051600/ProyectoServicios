a9a9e48d04566323b092cb20b1be29ec18cad3e8
Santiago Perez <santiago.grosso051600@hotmail.com>
2021-09-21 19:50:20 -0500

    Servicios

--- app/src/main/java/com/example/proyectoservicios/Servicio.java
+++ app/src/main/java/com/example/proyectoservicios/Servicio.java
@@ -2,14 +2,67 @@ package com.example.proyectoservicios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
-public class Servicio extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
 
+public class Servicio extends AppCompatActivity implements View.OnClickListener {
+    Button btn_fecha;
+    Button btn_hora;
+    EditText txt_fecha,txt_hora;
+    private int dia,mes,anio,hora,minuto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicio);
+        btn_fecha=(Button) findViewById(R.id.btn_fecha);
+        btn_hora=(Button) findViewById(R.id.btn_hora);
+        txt_fecha= (EditText) findViewById(R.id.txt_fecha);
+        txt_hora=(EditText) findViewById(R.id.txt_hora);
+
+        btn_fecha.setOnClickListener( this);
+        btn_hora.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btn_fecha){
+            final Calendar C = Calendar.getInstance();
+            dia=C.get(Calendar.DAY_OF_MONTH);
+            mes=C.get(Calendar.MONTH);
+            anio=C.get(Calendar.YEAR);
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,new  DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfYear){
+                    txt_fecha.setText(dayOfYear+"/"+monthOfYear+"/"+year);
+                }
+            },dia,mes,anio);
+            datePickerDialog.show();
+        }
+        if(v==btn_hora){
+            final Calendar C = Calendar.getInstance();
+            hora = C.get(Calendar.HOUR_OF_DAY);
+            minuto = C.get(Calendar.MINUTE);
+
+            TimePickerDialog tp = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int HourOfDay, int minute) {
+                    txt_hora.setText(HourOfDay+":"+minute);
+                }
+            },hora,minuto,false);
+            tp.show();
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/proyectoservicios/VentanaServicios.java
+++ app/src/main/java/com/example/proyectoservicios/VentanaServicios.java
@@ -3,8 +3,10 @@ package com.example.proyectoservicios;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VentanaServicios extends AppCompatActivity {
     LinearLayout contenedor;
@@ -37,14 +41,9 @@ public class VentanaServicios extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                lista.add(new boton(1,"hola"));
-
                 if(snapshot.exists()){
                     for (DataSnapshot ds: snapshot.getChildren()) {
-
                         lista.add(new boton(1,""+ds.child("nombre").getValue().toString()));
-
-
                     }
                     for(boton b:lista){
                         Button bt = new Button(getApplicationContext());
@@ -58,6 +57,7 @@ public class VentanaServicios extends AppCompatActivity {
                                 String aux;
                                 aux = bt.getText().toString();
                                 Log.d("STATE",""+aux);
+                                siguiente(view);
 
                             }
                         });
@@ -68,18 +68,83 @@ public class VentanaServicios extends AppCompatActivity {
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                lista.add(new boton(1,"hola"));
+
             }
         });
 
 
 
     }
+    public void siguiente(View view){
+        Intent siguiente = new Intent(this, Servicio.class);
+        startActivity(siguiente);
+
+    }
     private  void getServiciosFromFirebase(){
 
     }
     private  void setServiciosFromFirebase(){
-        Database.child("servicios").push().child("nombre").setValue("Baño y peluquería");
+        Map<String,String>servicios = new HashMap<>();
+        servicios.put("nombre","Vacunas");
+        servicios.put("precio","$30.000");
+        servicios.put("Descripcion","El precio varia segun la vacuna que se requiera para el animal");
+        Database.child("servicios").push().setValue(servicios);
+        ///////////////////////////////////////////
+        Map<String,String>servicios1 = new HashMap<>();
+        servicios1.put("nombre","Peluqueria");
+        servicios1.put("precio","$45.000");
+        servicios1.put("Descripcion","Se realiza todo tipo de corte segun el pelo de su mascota");
+        Database.child("servicios").push().setValue(servicios1);
+        ///////////////////////////////////////////
+        Map<String,String>servicios2 = new HashMap<>();
+        servicios2.put("nombre","Corte De Uñas");
+        servicios2.put("precio","$5.000");
+        servicios2.put("Descripcion","Se corta y se liman");
+        Database.child("servicios").push().setValue(servicios2);
+        ///////////////////////////////////////////
+        Map<String,String>servicios3 = new HashMap<>();
+        servicios3.put("nombre","Urgencia");
+        servicios3.put("precio","$0");
+        servicios3.put("Descripcion","El precio final depende del procedimiento realizado en la mascota");
+        Database.child("servicios").push().setValue(servicios3);
+        ///////////////////////////////////////////
+        Map<String,String>servicios4 = new HashMap<>();
+        servicios4.put("nombre","Labaratorio clinico");
+        servicios4.put("precio","$0");
+        servicios4.put("Descripcion","El precio final depende del procedimiento realizado en la mascota");
+        Database.child("servicios").push().setValue(servicios4);
+        ///////////////////////////////////////////
+        Map<String,String>servicios5 = new HashMap<>();
+        servicios5.put("nombre","Consulta Medica General");
+        servicios5.put("precio","$45.000");
+        servicios5.put("Descripcion","Pago unica vez");
+        Database.child("servicios").push().setValue(servicios5);
+        ///////////////////////////////////////////
+        Map<String,String>servicios6 = new HashMap<>();
+        servicios6.put("nombre","Desparacitacion");
+        servicios6.put("precio","$20.000");
+        servicios6.put("Descripcion","Dependiendo del peso de la mascota se suministra la cantidad");
+        Database.child("servicios").push().setValue(servicios6);
+        ///////////////////////////////////////////
+        Map<String,String>servicios7 = new HashMap<>();
+        servicios7.put("nombre","Baño");
+        servicios7.put("precio","$40.000");
+        servicios7.put("Descripcion","El baño incluye corte de uñas y limpieza de oidos");
+        Database.child("servicios").push().setValue(servicios7);
+        ///////////////////////////////////////////
+        Map<String,String>servicios8 = new HashMap<>();
+        servicios8.put("nombre","Radiologia");
+        servicios8.put("precio","$80.000");
+        servicios8.put("Descripcion","Se realiza un procedimiento de rayos x");
+        Database.child("servicios").push().setValue(servicios8);
+        ///////////////////////////////////////////
+        Map<String,String>servicios9 = new HashMap<>();
+        servicios9.put("nombre","Odontologia");
+        servicios9.put("precio","$50.000");
+        servicios9.put("Descripcion","El procedimiento incluye la anestecia");
+        Database.child("servicios").push().setValue(servicios9);
+        ///////////////////////////////////////////
+
     }
     class boton{
         public int codigo;
