f89e9fea95b192fecdb214ae619e530d87ff5498
Santiago Perez <santiago.grosso051600@hotmail.com>
2021-11-10 19:46:48 -0500

    preguntas

---
+++ app/src/main/java/com/example/proyectoservicios/preguntas_Frecuentes.java
@@ -0,0 +1,14 @@
+package com.example.proyectoservicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class preguntas_Frecuentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preguntas_frecuentes);
+    }
+}
